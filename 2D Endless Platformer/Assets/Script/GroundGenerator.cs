7d934306243f1a2e27f57abc380419229611de5d
TaeXtream <59652919+TaeXtream@users.noreply.github.com>
2022-03-06 14:13:25 +0700

    Improve on Ground Generator Script
    Add script to remove ground that player already pass

--- 2D Endless Platformer/Assets/Script/GroundGenerator.cs
+++ 2D Endless Platformer/Assets/Script/GroundGenerator.cs
@@ -5,19 +5,36 @@ using UnityEngine.Playables;
 
 public class GroundGenerator : MonoBehaviour
 {
-    private const float PLAYER_DISTANCE_SPAWN_GROUND = 100f;
+    private const float PLAYER_DISTANCE_SPAWN_GROUND = 50f;
+
+    private const int MAX_GROUND = 4;
     [SerializeField] private Transform groundPart;
     [SerializeField] private GameObject player;
 
     private Vector3 lastGroundPosition;
+
+    private List<GameObject> groundList;
     private void Awake() {
         lastGroundPosition = groundPart.Find("GroundEndposition").position;
+        groundList = new List<GameObject>();
         SpawnGround();
     }
 
     private void Update() {
         if (Vector3.Distance(player.transform.position, lastGroundPosition) < PLAYER_DISTANCE_SPAWN_GROUND) {
             SpawnGround();
+        }
+        if (groundList.Count > MAX_GROUND)
+        {
+            DestroyGround();
+        }
+    }
+
+    private void DestroyGround() {
+        for (int i = 0; i < 2; i++) {
+            GameObject ground = groundList[i];
+            groundList.RemoveAt(i);
+            Destroy(ground);
         }
     }
 
@@ -28,6 +45,7 @@ public class GroundGenerator : MonoBehaviour
 
     private Transform SpawnGround(Vector3 spawnPosition) {
         Transform groundTranform = Instantiate(groundPart, spawnPosition, Quaternion.identity);
+        groundList.Add(groundTranform.gameObject);
         return groundTranform;
     }
 }
