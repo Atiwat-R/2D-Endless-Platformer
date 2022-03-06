8eea4c4e793dced1f4e50ffb395e48368c1cd5fa
TaeXtream <59652919+TaeXtream@users.noreply.github.com>
2022-03-06 13:39:36 +0700

    Implement working Ground Generator

---
+++ 2D Endless Platformer/Assets/Script/GroundGenerator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class GroundGenerator : MonoBehaviour
+{
+    private const float PLAYER_DISTANCE_SPAWN_GROUND = 100f;
+    [SerializeField] private Transform groundPart;
+    [SerializeField] private GameObject player;
+
+    private Vector3 lastGroundPosition;
+    private void Awake() {
+        lastGroundPosition = groundPart.Find("GroundEndposition").position;
+        SpawnGround();
+    }
+
+    private void Update() {
+        if (Vector3.Distance(player.transform.position, lastGroundPosition) < PLAYER_DISTANCE_SPAWN_GROUND) {
+            SpawnGround();
+        }
+    }
+
+    private void SpawnGround() {
+        Transform lastGroundTranform = SpawnGround(lastGroundPosition);
+        lastGroundPosition = lastGroundTranform.Find("GroundEndposition").position;
+    }
+
+    private Transform SpawnGround(Vector3 spawnPosition) {
+        Transform groundTranform = Instantiate(groundPart, spawnPosition, Quaternion.identity);
+        return groundTranform;
+    }
+}
