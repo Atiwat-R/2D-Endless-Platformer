5f42786f148a9f35dbb514379dfe83419a475873
Tan Kongpithaksilp <45220855+tangentino@users.noreply.github.com>
2022-03-13 03:22:30 +0700

    Added score system

---
+++ 2D Endless Platformer/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+
+    public float scoreCount; 
+
+    public float pointsPerSecond;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreCount += pointsPerSecond * Time.deltaTime;
+        
+        scoreText.text = "Score: " + Mathf.Round(scoreCount);
+    }
+}
