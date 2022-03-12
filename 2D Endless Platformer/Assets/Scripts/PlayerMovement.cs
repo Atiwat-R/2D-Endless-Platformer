0a6c6dc0e847182e75c83ff25fd03df11208943c
Tan Kongpithaksilp <45220855+tangentino@users.noreply.github.com>
2022-03-12 18:51:52 +0700

    Add jumping animation

--- 2D Endless Platformer/Assets/Scripts/PlayerMovement.cs
+++ 2D Endless Platformer/Assets/Scripts/PlayerMovement.cs
@@ -11,25 +11,37 @@ public class PlayerMovement : MonoBehaviour
     CapsuleCollider2D myCapsuleCollider;
     Rigidbody2D rigidbody2D;
 
+    Animator myAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         myCapsuleCollider = GetComponent<CapsuleCollider2D>();
-
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Airborne();
+    }
+
+    // If player is airborne play animation
+    void Airborne() {
+        bool playerIsAirborne = rigidbody2D.velocity.y > Mathf.Epsilon;
         
+        myAnimator.SetBool("isAirborne", playerIsAirborne);
     }
 
     // Jump using Keyboard Spacebar
     void OnJump(InputValue value)
     {
         if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")) && !myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Platform"))) return;
-        if (value.isPressed) rigidbody2D.velocity += new Vector2(0f, jumpSpeed * 2.0f); // Jump height boost
+        if (value.isPressed) {
+             rigidbody2D.velocity += new Vector2(0f, jumpSpeed * 2.0f); // Jump height boost
+             myAnimator.SetTrigger("isJumping");
+        }
     }
     // Dash using Keyboard D
     void OnDash(InputValue value)
