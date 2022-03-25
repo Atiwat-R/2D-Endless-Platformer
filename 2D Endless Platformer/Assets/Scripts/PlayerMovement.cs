a5ca347840d46e57ca916ab4ea5213ec727e4b92
Atiwat-R <75544677+Atiwat-R@users.noreply.github.com>
2022-03-25 21:45:45 +0700

    Added Player Death
    - Remove PlayerController.cs (it was unused)
    - Now whenever the player hits enemy or obstacle, they are transported to GameOver scene.

--- 2D Endless Platformer/Assets/Scripts/PlayerMovement.cs
+++ 2D Endless Platformer/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -61,8 +62,10 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    // Implements GAME OVER
     void TakeDamage() {
         Debug.Log("Ouch");
+        SceneManager.LoadScene("GameOver"); // Sends Player to Game Over scene
     }
 
 }
