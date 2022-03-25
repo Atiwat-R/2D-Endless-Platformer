4cdcdea497b2913914751bc25f437a05b130ce65
Atiwat-R <75544677+Atiwat-R@users.noreply.github.com>
2022-03-25 21:41:03 +0700

    Added Game Over Scene
    - Added GameOver scene that allows retry or links back to Menu scene.

--- 2D Endless Platformer/Assets/Scripts/UI Scripts/GameOverSceneController.cs
+++ 2D Endless Platformer/Assets/Scripts/UI Scripts/GameOverSceneController.cs
@@ -1,20 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
-using UnityEngine.UI;
-
 using UnityEngine.SceneManagement;
 
-public class MenuSceneUIController : MonoBehaviour
+public class GameOverSceneController : MonoBehaviour
 {
 
+    // Go to Game Scene
 	public void LoadMainScene(){
 		SceneManager.LoadScene("Main");
 	}
 
-
+    // Go to Menu Scene
+    public void LoadMenuScene(){
+		SceneManager.LoadScene("Menu");
+	}
 
 }
-
