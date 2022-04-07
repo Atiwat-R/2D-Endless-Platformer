b978de3e249329452a4a326f3a84a835c62da0ac
Atiwat-R <75544677+Atiwat-R@users.noreply.github.com>
2022-04-07 21:49:04 +0700

    Added High Score
    - Added counter for High Score shown at GameOver Scene

--- 2D Endless Platformer/Assets/Scripts/GameOverScore.cs
+++ 2D Endless Platformer/Assets/Scripts/GameOverScore.cs
@@ -7,13 +7,23 @@ public class GameOverScore : MonoBehaviour
 {
 
     public Text FinalScore;
+    public Text HighScore;
 
 
     void Start()
     {
         float toDisplay = PlayerPrefs.GetFloat("FinalScore");
+        float highScore = PlayerPrefs.GetFloat("HighScore"); // Initially 0, but gets updated
+
+        // Keep High Score
+        if (toDisplay > highScore) {
+            PlayerPrefs.SetFloat("HighScore", toDisplay);
+        }
+
+        // Show this Game's Score
         FinalScore.text = "SCORE:" + Mathf.Round(toDisplay);
-        Debug.Log(toDisplay);
+        // Show HighScore
+        HighScore.text = "HIGH SCORE:" + Mathf.Round(PlayerPrefs.GetFloat("HighScore"));
     }
 
 
